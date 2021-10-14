c9a54ba7b7a52713d895bd0d481376e279319746
trevormedley <trevormedley10@gmail.com>
2021-10-14 13:20:21 -0400

    Finished beginner problems

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -6,7 +6,8 @@ var arr = [10,20,30,40,50,60]
   Create a variable named 'firstItem' and set it equal to the first value of the 'arr' array.
 */
 
-//Code Here
+firstItem = arr[0];
+console.log(firstItem);
 
 
 ////////// PROBLEM 2 //////////
@@ -14,7 +15,8 @@ var arr = [10,20,30,40,50,60]
   Remove the last item from the 'arr' array and store it in a variable named 'lastItemRemoved'. Console.log the array to verify you no longer have the last item.
 */
 
-//Code Here
+lastItemRemoved = arr.pop()
+console.log(lastItemRemoved);
 
 ////////// PROBLEM 3 //////////
 
@@ -26,7 +28,9 @@ var family = ['Tyler', 'Jordan', 'Ryan', 'Alice', 'Ireland'];
   Loop through the 'family' array and console.log every item.
 */
 
-//Code Here
+for (let i = 0; i < family.length; i++) {
+  console.log(family[i]); 
+}
 
 
 
@@ -41,9 +45,12 @@ let evensArr = []
   Use a for-loop to iterate through each number in the 'nums' array, if the number is even, append it to the 'evensArr'.
 */
 
-//Code Here
-
-
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] % 2 === 0) {
+    evensArr.push(nums[i]);
+  }
+}
+console.log(evensArr);
 
 ////////// PROBLEM 5 //////////
 
@@ -55,8 +62,19 @@ var score = 74
   Determine if the letter grade of the given variable 'score'. If the variable is a 90 or above, console log an 'A', between 80 and 89, console log a 'B', between 70 and 79, 'C', between 60 and 69, 'D', and anything below 60 should console log an 'F'.
 */
 
-//Code Here
-
+if (score >= 90) {
+  console.log('A');
+} else if (score >= 80 && score <= 89) {
+  console.log('B');
+} else if (score >= 70 && score <= 79) {
+  console.log('C');
+} else if (score >= 60 && score <= 69) {
+  console.log('D');
+} else if (score < 60) {
+  console.log('F');
+} else {
+  console.log('Invalid Input')
+}
 
 ////////// Intermediate Problems //////////
 
@@ -70,7 +88,8 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
   Create a variable named 'someNum' and set it equal to the fifth value of the 'myFavoriteNumbers' array.
 */
 
-//Code Here
+let someNum = myFavoriteNumbers[4];
+console.log(someNum);
 
 
 ////////// PROBLEM 7 //////////
